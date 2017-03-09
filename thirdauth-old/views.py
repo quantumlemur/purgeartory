1db396f5211ac417034fe26ad854ebbd191d748b
quantumlemur <quantumlemur@gmail.com>
2017-03-09 15:44:07 -0500

    removed old files

--- thirdauth-old/views.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
-# @login_required
-def home(request):
-    return render(request, 'thirdauth/home.html')
